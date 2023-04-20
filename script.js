d1b24b1fd9ff1999e86226cb00dcc017c017961f
Gels04 <gelcira2004@outlook.com>
2023-04-20 14:18:53 +0100

    toggle lightbulbs

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // Write your code here
+const lightBulb1 = document.querySelector("#lightbulb1");
+const lightBulb2 = document.querySelector("#lightbulb2");
+const lightBulb3 = document.querySelector("#lightbulb3");
+const subtitle = document.querySelector("h3.subtitle");
+
+let count = 0;
+function events() {
+    count ++
+    subtitle.innerHTML = `You've clicked the lights ${count} times`
+    this.classList.toggle("active");
+}
+
+lightBulb1.addEventListener("click", events);
+lightBulb2.addEventListener("click", events);
+lightBulb3.addEventListener("click", events);
